ba2ab038edfc153bbd1ce75402844bf53a9bc60d
SASWAT6768 <61114228+SASWAT6768@users.noreply.github.com>
2020-10-16 10:06:40 +0530

    Update GCD.s

--- Assignment1/GCD.s
+++ Assignment1/GCD.s
@@ -2,9 +2,9 @@
      EXPORT __main
 	 ENTRY 
 __main  FUNCTION		         
-        	MOV R0,#42			;	A=32
-		    MOV R1,#12			;	B=16
-		    MOV R2,#0x20000000	;	
+        	MOV R0,#42			;	take any values for A,B A=42
+		    MOV R1,#12			;	B=12
+		    MOV R2,#0x10000000	;	        address for storing largest no.
 		
 WHILE		CMP R0,R1		;	While(A!=B)
 			BEQ ENDLOOP		;
@@ -17,7 +17,7 @@ ELSELOOP	SUB R1,R1,R0	;	B=B-A
 			B WHILE			;
 			
  
-ENDLOOP		STR R0,[R2]		;	Store data back to 0x20000000
+ENDLOOP		STR R0,[R2]		;	Store data back to 0x10000000
 		 
      ENDFUNC
      END
