007d957f0e222c19e434f46f9aa98fdb6689fb8b
SASWAT6768 <61114228+SASWAT6768@users.noreply.github.com>
2020-10-16 10:03:17 +0530

    Update Fibonacci.s

--- Assignment1/Fibonacci.s
+++ Assignment1/Fibonacci.s
@@ -1,32 +1,31 @@
-    THUMB
-    AREA     appcode, CODE, READONLY
+   AREA     appcode, CODE, READONLY
     EXPORT __main
 	 ENTRY 
 __main  FUNCTION		         
-    MOV R0,#0			;SETTING FIRST 	VALUE
-		MOV R1,#1			;SETTING SECOND VALUE
-		MOV R3,#0x20000000	;Address for storing Fibonacci values
-		MOV R4,#10			;The Nth number in Fibonacci series;here its is given as 10th no.
+                MOV R0,#0;		INITIALISIING FIRST 	VALUE
+		MOV R1,#1;		SETTING SECOND VALUE
+		MOV R3,#0x10000000;	Address for storing Fibonacci values
+		MOV R4,#10;		The Nth number in Fibonacci series;here its is given as 10th no.
 		
-		CMP r4,#1           ;
-		BNE elsex           ;		if (R4==1)          
+		CMP R4,#1;           	check if (R4==1) 
+		BNE elsex;		         
 		
-		MOV R2,R0           ;		R2=R0
-		B endx              ;
+		MOV R2,R0;		R2=R0
+		B endx;           
 
-elsex 	CMP r4,#2           ;		else if(R4==2)
-		BNE elsexx          ;     
-		MOV R2,R1           ;
-		B endx              ;
-elsexx 	SUB R4,R4,#1        ;		Fibonacci values added to R2
-		ADD R2,R1,R0        ;
-		MOV R0,R1           ;
-		MOV R1,R2           ;
-		CMP R4,#2           ;
-		BNE elsexx          ; 
+elsex 	        CMP r4,#2;		else if(R4==2)
+		BNE elsexx;     
+		MOV R2,R1;
+		B endx;
+elsexx    	SUB R4,R4,#1;		Fibonacci values added to R2
+		ADD R2,R1,R0;
+		MOV R0,R1;
+		MOV R1,R2;
+		CMP R4,#2;
+		BNE elsexx; 
 endx	
-		STR R2,[R3]			;		R2 storred in 0x20000000
-stop B stop; stop program		
+		STR R2,[R3];		R2 storred in 0x20000000
+stop B stop; 				stop program		
      ENDFUNC
      END
 
